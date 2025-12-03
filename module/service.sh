afe0b21a4ddcc982a4b1441c90dfd0f7acf3f05c
BasGame1 <abadbasantaalex@gmail.com>
2025-12-03 20:05:29 +0100

    module: service: fix dialer props

--- module/service.sh
+++ module/service.sh
@@ -115,8 +115,7 @@ if [ -d "$DATA_PATH_USER" ]; then
       chmod -R 0700 "$DATA_PATH_DATA"
     fi
   fi
-  
+fi
 # Enable iso spoofing
 resetprop -n gsm.operator.iso-country us,
 resetprop -n gsm.sim.operator.iso-country us,
-fi
