3817a9be600265ee71c8459016cb3685074c2426
BasGame1 <abadbasantaalex@gmail.com>
2025-12-16 15:04:53 +0100

    module: service: Fix error

--- module/service.sh
+++ module/service.sh
@@ -1,12 +1,5 @@
 #!/system/bin/sh
-# Do NOT assume where your module will be located.
-# ALWAYS use $MODDIR if you need to know where this script
-# and module is placed.
-# This will make sure your module will still work
-# if Magisk change its mount point in the future
 MODDIR=${0%/*}
-
-# This script will be executed in late_start service mode
 
 . $MODDIR/vars.sh
 . $MODDIR/utils.sh
@@ -116,6 +109,10 @@ if [ -d "$DATA_PATH_USER" ]; then
     fi
   fi
 fi
-# Enable iso spoofing
+# Enable global spoofing
 resetprop -n gsm.operator.iso-country us,
 resetprop -n gsm.sim.operator.iso-country us,
+resetprop -n persist.sys.country us
+resetprop -n ro.product.locale.region US
+resetprop -n ro.product.locale.language en
+resetprop -n persist.sys.language en
