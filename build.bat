0818b85169e5aff44c2de0c6c50c8dd4c55c3df9
BasGame1 <abadbasantaalex@gmail.com>
2026-02-27 23:06:05 +0100

    build (bat): Init

--- build.bat
+++ build.bat
@@ -1,1 +1,20 @@
-gradlew :zygisk:assembleRelease
+@echo off
+echo Select your build version
+echo 1 for BETA
+echo 2 for STABLE
+
+:: /p allows a prompt string before the user types
+set /p SELECTION="Enter your selection: "
+
+if "%SELECTION%"=="1" (
+    echo building BETA version
+    call gradlew.bat :beta:assembleDebug --no-configuration-cache
+) else if "%SELECTION%"=="2" (
+    echo building STABLE version
+    call gradlew.bat :stable:assembleDebug --no-configuration-cache
+) else (
+    echo No option selected, aborting
+    exit /b 1
+)
+
+pause
