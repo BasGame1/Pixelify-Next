15270218f5c8c30e49184c7a1244260679a1eae3
BasGame1 <abadbasantaalex@gmail.com>
2026-02-28 14:14:14 +0100

    colors: Add aux file

---
+++ colors.sh
@@ -0,0 +1,23 @@
+red() {
+ echo -e "\033[0;31m"
+ echo "$1"
+ echo -e "\033[0m"
+}
+
+blue() {
+ echo -e "\033[0;34m"
+ echo "$1"
+ echo -e "\033[0m"
+}
+
+green() {
+ echo -e "\033[0;32m"
+ echo "$1"
+ echo -e "\033[0m"
+}
+
+yellow() {
+ echo -e "\033[1;33m"
+ echo "$1"
+ echo -e "\033[0m"	
+}
