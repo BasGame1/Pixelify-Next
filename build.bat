138108d284e53bad919be1bb6463cef16b4a96e8
BasGame1 <abadbasantaalex@gmail.com>
2026-02-28 14:24:56 +0100

    build (bat): Add color

--- build.bat
+++ build.bat
@@ -1,32 +1,46 @@
 @echo off
-echo Select your build version
-echo 1 for BETA
-echo 2 for STABLE
+setlocal enabledelayedexpansion
 
-:: /p allows a prompt string before the user types
-set /p SELECTION="Enter your selection: "
-
-if "%SELECTION%"=="1" (
-    echo building BETA version
-    echo ""
-    echo Building VK version
-    call gradlew :beta:zipRelease --no-configuration-cache
-    echo Building no VK version
-    call gradlew :beta:novkzipRelease --no-configuration-cache
-    echo Cleaning
-    call gradlew :beta:cleanDir --no-configuration-cache
-) else if "%SELECTION%"=="2" (
-    echo building STABLE version
-    echo ""
-    echo Building VK version
-    call gradlew :stable:zipRelease --no-configuration-cache
-    echo Building no VK version
-    call gradlew :stable:novkzipRelease --no-configuration-cache
-    echo Cleaning
-    call gradlew :stable:cleanDir --no-configuration-cache
+if exist "colors.bat" (
+    call colors.bat
 ) else (
-    echo No option selected, aborting
+    echo Colors file could not be loaded
     exit /b 1
 )
 
-pause
+echo %YELLOW%Select your build version%RESET%
+echo %RED%1 for BETA%RESET%
+echo %GREEN%2 for STABLE%RESET%
+
+set /p SELECTION="Enter your selection: "
+
+if "%SELECTION%"=="1" (
+    echo %RED%building BETA version%RESET%
+    echo.
+    echo %RED%Building VK version%RESET%
+    call gradlew :beta:zipRelease --no-configuration-cache
+    
+    echo %RED%Building no VK version%RESET%
+    call gradlew :beta:novkzipRelease --no-configuration-cache
+    
+    echo %BLUE%Cleaning%RESET%
+    call gradlew :beta:cleanDir --no-configuration-cache
+    goto :EOF
+)
+
+if "%SELECTION%"=="2" (
+    echo %GREEN%building STABLE version%RESET%
+    echo.
+    echo %GREEN%Building VK version%RESET%
+    call gradlew :stable:zipRelease --no-configuration-cache
+    
+    echo %GREEN%Building no VK version%RESET%
+    call gradlew :stable:novkzipRelease --no-configuration-cache
+    
+    echo %BLUE%Cleaning%RESET%
+    call gradlew :stable:cleanDir --no-configuration-cache
+    goto :EOF
+)
+
+echo %BLUE%No option selected, aborting%RESET%
+exit /b 1
