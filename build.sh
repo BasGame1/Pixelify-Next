0a64096cb34e1997afc36e9cfb220c8e42a981bd
BasGame1 <abadbasantaalex@gmail.com>
2026-02-28 14:17:05 +0100

    build: Add color

--- build.sh
+++ build.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
-echo "Select your build version"
-echo "1 for BETA"
-echo "2 for STABLE"
+source colors.sh || echo "Colors file could not be loaded"
+
+yellow "Select your build version"
+red "1 for BETA"
+green "2 for STABLE"
 read -p "Enter your selection: " SELECTION
 case $SELECTION in
 "1")
- echo "building BETA version"
+ red "building BETA version"
+ echo ""
+ red "Building VK version"
  ./gradlew :beta:zipRelease --no-configuration-cache
+ red "Building no VK version"
  ./gradlew :beta:novkzipRelease --no-configuration-cache
+ blue "Cleaning"
+ ./gradlew :beta:cleanDir --no-configuration-cache
  ;;
 "2")
- echo "building STABLE version"
+ green "building STABLE version"
+ echo ""
+ green "Building VK version"
  ./gradlew :stable:zipRelease --no-configuration-cache
+ green "Building no VK version"
  ./gradlew :stable:novkzipRelease --no-configuration-cache
+ blue "Cleaning"
+ ./gradlew :stable:cleanDir --no-configuration-cache
  ;;
 *)
- echo "No option selected, aborting"
+ blue "No option selected, aborting"
  exit 1
  ;;
 esac
