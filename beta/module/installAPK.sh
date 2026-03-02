297b66d8d2d6bf15ab1d37ce335a7310eb363af3
BasGame1 <abadbasantaalex@gmail.com>
2026-03-02 18:09:37 +0100

    beta: module: installapk: Add abi as arg

--- beta/module/installAPK.sh
+++ beta/module/installAPK.sh
@@ -3,6 +3,7 @@
 
 APK_NAME=$1
 APK_PATH=$2
+ABI=$3
 APK_CUT=$(echo $APK_NAME | cut -d '.' -f 2)
 DOT_COUNT=$(echo $APK_NAME | grep -Fo "." | wc -m) 
 TMP=$MODPATH/tmp
@@ -10,7 +11,7 @@ TOTAL_SIZE=0
 I=0
 
 if [[ "$APK_NAME" == '-h' ]] || [[ "$APK_NAME" = "help" ]]; then
- echo -e "HELP MENU \n use example: ./installAPK [APK_NAME] [APK_PATH] \n -h or help shows this menu"
+ echo -e "HELP MENU \n use example: ./installAPK [APK_NAME] [APK_PATH] [ABI - SELECT ARM64 OR ARM32]\n -h or help shows this menu"
  exit 0
 fi
 
@@ -21,15 +22,20 @@ fi
 
 if [ "$APK_CUT" = "apk" ]; then
  echo "normal APK detected, installing $APK_NAME in $APK_PATH"
+ if [ "$ABI" == "arm64" ]; then
+  echo "Installing with ABI arm64"
+  pm install --abi arm64-v8a $APK_PATH
+ elif [ "$ABI" == "arm32" ]; then
+  echo "Installing with ABI arm32"
+  pm install --abi armabi-v7a $APK_PATH
+ else
+ echo "Installing without ABI"
  pm install $APK_PATH
-
+ fi
 elif [ "$APK_CUT" = "apks" ] || [ "$APK_CUT" = "apkm" ]; then
  echo "APK split detected, installing $APK_NAME in $APK_PATH"
  mkdir -p $TMP
  mv $APK_PATH $TMP/$APK_NAME.zip
- # DEBUG: make a backup
- cp $TMP/$APK_NAME.zip $TMP/..
- # DEBUG
  unzip -o $TMP/$APK_NAME.zip -d $TMP
  rm -rf $TMP/$APK_NAME.zip
  
@@ -39,8 +45,18 @@ elif [ "$APK_CUT" = "apks" ] || [ "$APK_CUT" = "apkm" ]; then
    TOTAL_SIZE=$(( $TOTAL_SIZE + $SIZE ))
    
  done
-
- SESSION=$(pm install-create -S $TOTAL_SIZE | grep -Eo '[0-9]+')
+ 
+ if [ "$ABI" == "arm64" ]; then
+  echo "Creating session with ABI arm64"
+  SESSION=$(pm install-create --abi arm64-v8a -S $TOTAL_SIZE | grep -Eo '[0-9]+')
+ elif [ "$ABI" == "arm64" ]; then
+  echo "Creating session with ABI arm32"
+  SESSION=$(pm install-create --abi armabi-v7a -S $TOTAL_SIZE | grep -Eo '[0-9]+')
+ else
+  echo "Creating session without ABI"
+  SESSION=$(pm install-create -S $TOTAL_SIZE | grep -Eo '[0-9]+')
+ fi
+ 
  echo "Session created with ID $SESSION and size $TOTAL_SIZE"
 
 for APK in $MODPATH/tmp/*.apk; do
