5473fcf7a51d372411131110aa8b843ff85becd4
BasGame1 <abadbasantaalex@gmail.com>
2026-03-06 19:16:10 +0100

    beta: module: customize: Add remove option for XML files of pixel launcher & Fix style

--- beta/module/customize.sh
+++ beta/module/customize.sh
@@ -1796,30 +1796,24 @@ if [ $API -ge 36 ]; then
                 print ""
                 unzip -o $MODPATH/system/product/priv-app/DevicePersonalizationPrebuiltPixel2025/DevicePersonalizationPrebuiltPixel2025.zip $MODPATH/system/product/priv-app/DevicePersonalizationPrebuiltPixel2025
                 set_perm_recursive $MODPATH/system/etc 0 0 0755 0644
-		set_perm_recursive $MODPATH/system/product/app 0 0 0755 0644
-		set_perm_recursive $MODPATH/system/product/etc 0 0 0755 0644
-		set_perm_recursive $MODPATH/system/product/media 0 0 0755 0644
-		set_perm_recursive $MODPATH/system/product/overlay 0 0 0755 0644
-		set_perm_recursive $MODPATH/system/product/priv-app 0 0 0755 0644
-		set_contexts $MODPATH/system/etc/sysconfig u:object_r:system_sysconfig_file:s0
-		set_contexts $MODPATH/system/product/app u:object_r:product_app_file:s0
-		set_contexts $MODPATH/system/product/etc/permissions u:object_r:product_etc_file:s0
-		set_contexts $MODPATH/system/product/media u:object_r:product_media_file:s0
-		set_contexts $MODPATH/system/product/overlay u:object_r:product_overlay_file:s0
-		set_contexts $MODPATH/system/product/priv-app u:object_r:product_priv_app_file:s0
+                set_perm_recursive $MODPATH/system/product/app 0 0 0755 0644
+                set_perm_recursive $MODPATH/system/product/etc 0 0 0755 0644
+                set_perm_recursive $MODPATH/system/product/media 0 0 0755 0644
+                set_perm_recursive $MODPATH/system/product/overlay 0 0 0755 0644
+                set_perm_recursive $MODPATH/system/product/priv-app 0 0 0755 0644
+                set_contexts $MODPATH/system/etc/sysconfig u:object_r:system_sysconfig_file:s0
+                set_contexts $MODPATH/system/product/app u:object_r:product_app_file:s0
+                set_contexts $MODPATH/system/product/etc/permissions u:object_r:product_etc_file:s0
+                set_contexts $MODPATH/system/product/media u:object_r:product_media_file:s0
+                set_contexts $MODPATH/system/product/overlay u:object_r:product_overlay_file:s0
+                set_contexts $MODPATH/system/product/priv-app u:object_r:product_priv_app_file:s0
                 REMOVE="$REMOVE $PL $TR $QS $LW $TW $KW"
-                    # CTS removed bc the apk had 2/65 on virustotal
-                    #print "  Do you want to install Circle 2 search? (Stock rom only)"
-                    #print "  APk shared by"
-                    #print "   Vol Up += Yes"
-                    #print "   Vol Down += No"
-                    #no_vk "ENABLE_CTS"
-                    #if $VKSEL; then
-                    #    pm install $MODPATH/files/CTS.apk
-                    #fi
         else
             echo " - Deleting Pixel Launcher" >>$logfile
             for FILE in $MODPATH/system/*.apk; do
+                rm -rf $FILE 2>/dev/null
+            done
+            for FILE in $MODPATH/system/product/etc/permissions/com.android.systemui.plugin.globalactions.wallet.xml $MODPATH/system/product/etc/permissions/com.android.systemui.plugin.globalactions.wallet.xml $MODPATH/system/product/etc/permissions/com.google.android.apps.nexuslauncher.xml $MODPATH/system/product/etc/permissions/com.google.android.apps.wallpaper.xml $MODPATH/system/product/etc/permissions/com.google.android.apps.weather.xml $MODPATH/system/product/etc/permissions/privapp-permissions-com.google.android.apps.nexuslauncher.xml; do
                 rm -rf $FILE 2>/dev/null
             done
             rm -rf $MODPATH/system/product/priv-app/DevicePersonalizationPrebuiltPixel2025
