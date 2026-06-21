7bdbfbfd9fad4224436508bf131bfe1fe87ffa98
BasGame1 <abadbasantaalex@gmail.com>
2026-06-21 14:11:45 +0200

    beta: module: install apps: Make function to install now playing and journal

---
+++ beta/module/install_apps.sh
@@ -0,0 +1,30 @@
+install_apps() {
+if [ -d /data/data/com.google.android.googlequicksearchbox ] && [ $API -ge 29 ] && [ $TARGET_DEVICE_ONEUI -eq 0 ]; then
+    blue "  Google is installed."
+    log "  Do you want to installed Journal app?"
+    log "   Vol Up += Yes"
+    log "   Vol Down += No"
+    no_vk "INSTALL_JOURNAL"
+    if $VKSEL; then
+        echo " - Installing Google Journal" >>$logfile
+        log "- Installing Google Journal"
+        log ""
+        install_apk "Journal.apk" "$MODPATH/files/Journal.apk"
+     fi
+fi
+
+if [ -d /data/data/com.google.android.googlequicksearchbox ] && [ $API -ge 36 ]; then
+    log "  Google is installed."
+    log "  Do you want to installed Now Playing?"
+    log "  (needs aicore and device intelligence)"
+    log "   Vol Up += Yes"
+    log "   Vol Down += No"
+    no_vk "QUICK_SHARE"
+    if $VKSEL; then
+        log " - Installing Now Playing " >>$logfile
+        log "- Installing Now Playing "
+        log ""
+        install_apk "nowplaying.apkm" "$MODPATH/files/nowplaying.apkm" "arm64"
+     fi
+fi
+}
