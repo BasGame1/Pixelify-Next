868767848040db160a67123e09ecc692fccceeb8
BasGame1 <160914105+BasGame1@users.noreply.github.com>
2026-08-20 15:48:28 +0200

    beta: module: service: Fix bootloop detector typo in CPU ABI property

--- beta/module/service.sh
+++ beta/module/service.sh
@@ -56,7 +56,7 @@ check() {
 sleep 5
 
 MAIN_ZYGOTE_NICENAME=zygote
-CPU_ABI=$(getprop ro.product.cpu.api)
+CPU_ABI=$(getprop ro.product.cpu.abi)
 [ "$CPU_ABI" = "arm64-v8a" -o "$CPU_ABI" = "x86_64" ] && MAIN_ZYGOTE_NICENAME=zygote64
 
 ZYGOTE_PID1=$(pidof "$MAIN_ZYGOTE_NICENAME")
