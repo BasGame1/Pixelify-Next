a0be66d6f4f6ef1fb2954b35216cbc0c319c7076
BasGame1 <160914105+BasGame1@users.noreply.github.com>
2026-08-20 16:57:55 +0200

    build & colors: Add new look to build scripts

--- build.sh
+++ build.sh
@@ -1,34 +1,92 @@
 #!/bin/bash
 
-source colors.sh || echo "Colors file could not be loaded"
+set -e
 
+source colors.sh 2>/dev/null || echo -e "\033[0;31m[ERROR]\033[0m Colors file could not be loaded"
+
+user_option() {
 yellow "Select your build version"
-red "1 for BETA"
-green "2 for STABLE"
-read -p "Enter your selection: " SELECTION
+echo ""
+echo -e "\033[1;33m1) \033[0;34mBeta\033[0m"
+echo -e "\033[1;33m2) \033[0;34mStable\033[0m"
+echo ""
+read -n 1 -p "Enter your selection: " SELECTION
+echo ""
 case $SELECTION in
 "1")
- red "building BETA version"
+ info "building BETA version"
  echo ""
- red "Building VK version"
+ info "Building VK version"
  ./gradlew :beta:zipRelease --no-configuration-cache
- red "Building no VK version"
+ info "Building no VK version"
  ./gradlew :beta:novkzipRelease --no-configuration-cache
- blue "Cleaning"
+ info "Cleaning"
  ./gradlew :beta:cleanDir --no-configuration-cache
+ success "All done!"
+ exit
  ;;
 "2")
- green "building STABLE version"
- echo ""
- green "Building VK version"
+ info "building STABLE version"
+ info "Building VK version"
  ./gradlew :stable:zipRelease --no-configuration-cache
- green "Building no VK version"
+ info "Building no VK version"
  ./gradlew :stable:novkzipRelease --no-configuration-cache
- blue "Cleaning"
+ info "Cleaning"
  ./gradlew :stable:cleanDir --no-configuration-cache
+ success "All done!"
+ exit
  ;;
 *)
- blue "No option selected, aborting"
+ error "No option selected, aborting"
  exit 1
  ;;
 esac
+}
+if [ ! -z $1 ]; then
+while getopts "bsa" OPTIONS; do
+ case $OPTIONS in
+  b)
+   info "Beta: building VK version"
+   ./gradlew :beta:zipRelease --no-configuration-cache
+   info "Beta: building no VK version"
+   ./gradlew :beta:novkzipRelease --no-configuration-cache
+   info "Beta: cleaning"
+   ./gradlew :beta:cleanDir --no-configuration-cache
+   success "All Done!"
+  ;;
+  s)
+   info "Stable: building VK version"
+   ./gradlew :stable:zipRelease --no-configuration-cache
+   info "Stable: building no VK version"
+   ./gradlew :stable:novkzipRelease --no-configuration-cache
+   info "Stable: cleaning"
+   ./gradlew :stable:cleanDir --no-configuration-cache
+   success "All Done!"
+  ;;
+  a)
+   info "Building all"
+   info "Stable: building VK version"
+   ./gradlew :stable:zipRelease --no-configuration-cache
+   info "Stable: building no VK version"
+   ./gradlew :stable:novkzipRelease --no-configuration-cache
+   info "Stable: cleaning"
+   ./gradlew :stable:cleanDir --no-configuration-cache
+   info "Beta: building VK version"
+   ./gradlew :beta:zipRelease --no-configuration-cache
+   info "Beta: building no VK version"
+   ./gradlew :beta:novkzipRelease --no-configuration-cache
+   info "Beta: cleaning"
+   ./gradlew :beta:cleanDir --no-configuration-cache
+   success "All Done!"
+  ;;
+  *)
+   error "Unkown argument $OPTION, exiting"
+   exit 255
+  ;;
+ esac
+done
+else
+ info "No arguments, runnign decision"
+ user_option
+ exit
+fi
--- colors.sh
+++ colors.sh
@@ -1,23 +1,41 @@
 red() {
- echo -e "\033[0;31m"
- echo "$1"
+ echo -ne "\033[0;31m"
+ echo -n "$1"
  echo -e "\033[0m"
 }
 
 blue() {
- echo -e "\033[0;34m"
- echo "$1"
+ echo -ne "\033[0;34m"
+ echo -n "$1"
  echo -e "\033[0m"
 }
 
 green() {
- echo -e "\033[0;32m"
- echo "$1"
+ echo -ne "\033[0;32m"
+ echo -n "$1"
  echo -e "\033[0m"
 }
 
 yellow() {
- echo -e "\033[1;33m"
- echo "$1"
+ echo -ne "\033[1;33m"
+ echo -n "$1"
  echo -e "\033[0m"	
+}
+
+info() {
+ echo -ne "\033[1;33m"
+ echo -n "[INFO] $1"
+ echo -e "\033[0m"	
+}
+
+success() {
+ echo -ne "\033[0;32m"
+ echo -n "[SUCCESS] $1"
+ echo -e "\033[0m"
+}
+
+red() {
+ echo -ne "\033[0;31m"
+ echo -n "[ERROR] $1"
+ echo -e "\033[0m"
 }
